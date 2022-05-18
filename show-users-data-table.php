5700603b23d995d4414c5e6482d2f6216dbbd744
mauriac <azouamauriac@gmail.com>
2022-05-18 22:08:29 +0100

    Set up initial pattern

---
+++ show-users-data-table.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              #
+ * @since             1.0.0
+ * @package           shudat
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Show Users Data as Table
+ * Plugin URI:        #
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Mauriac Azoua
+ * Author URI:        #
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       shudat
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
