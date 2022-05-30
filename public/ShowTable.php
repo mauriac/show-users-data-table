d235298337a9ce99ddd11e675dae8ce5648b0f4f
mauriac <azouamauriac@gmail.com>
2022-05-30 10:13:03 +0100

    Sniff showtable.php

--- public/ShowTable.php
+++ public/ShowTable.php
@@ -5,40 +5,39 @@ declare(strict_types=1);
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<?php wp_head(); ?>
-</head>
-<body>
+    <head>
+        <?php wp_head(); ?>
+    </head>
 
-<h2><?php echo esc_html__('Users Table', 'shudat'); ?></h2>
+    <body>
+        <h2><?php echo esc_html__('Users Table', 'shudat'); ?></h2>
 
-<table>
-    <?php
-        $tableHead = ['id', 'name', 'username'];
-    ?>
-        <thead>
-            <tr>
-                <?php foreach ($tableHead as $key => $headValue) : ?>
-                    <th> <?php echo esc_html($headValue); ?> </th>
-                <?php endforeach; ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($args as $key => $user) : ?>
-                <tr data-userid=<?php echo esc_attr($user['id']); ?>>
-                <?php foreach ($tableHead as $key => $headValue) : ?>
-                    <td><a class="" href="#"><?php echo esc_html($user[ $headValue ]); ?></a></td>
-                <?php endforeach; ?>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-</table>
-<div id="shut_user_details" class=""></div>
-
-</body>
+        <div>
+            <table>
+                <?php
+                $tableHead = ['id', 'name', 'username'];
+                ?>
+                <thead>
+                    <tr>
+                        <?php foreach ($tableHead as $key => $headValue) : ?>
+                            <th> <?php echo esc_html($headValue); ?> </th>
+                        <?php endforeach; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($args as $key => $user) : ?>
+                        <tr data-userid=<?php echo esc_attr($user['id']); ?>>
+                            <?php foreach ($tableHead as $key => $headValue) : ?>
+                                <td><a class="" href="#"><?php echo esc_html($user[$headValue]); ?></a></td>
+                            <?php endforeach; ?>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <div id="shut_user_details" class=""></div>
+    </body>
 </html>
 <?php
 wp_footer();
 ?>
-
-
