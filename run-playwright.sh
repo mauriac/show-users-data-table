b5172212c7a24aed1e491317647063f86e7dd3fe
mauriac <azouamauriac@gmail.com>
2024-07-11 00:13:17 +0100

    Trigger test

--- run-playwright.sh
+++ run-playwright.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker compose run --rm playwright node plugin.test.js
+# docker compose run --rm playwright node plugin.test.js
+docker run -p 8000:8000 --rm --init -it mcr.microsoft.com/playwright:v1.45.1-jammy /bin/sh -c "cd ./ && npx -y playwright@1.41.0 run-server --port 8000 --host 0.0.0.0"
